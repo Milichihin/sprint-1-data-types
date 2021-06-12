67e6ea387df2d94854a12cca96d4dde167142e2a
Dima <milichihin@gmail.com>
2021-06-12 10:40:41 +0300

    try again

--- src/index.js
+++ src/index.js
@@ -10,3 +10,23 @@ document.getElementById("app").innerHTML = `
 SPRINT 1. Data types. Arrays. Functions. Exceptions.
 </div>
 `;
+
+//=== reduce()
+// The reduce() method performs the reducer function you specified for each element of the array and returns a single value.
+
+const data = [2, 4, 6];
+
+function reducer(total, value) {
+  return total / value;
+}
+console.log(data.reduce(reducer)); // 0.08333333333333333
+
+function reducer2(total, value) {
+  return total / value - value;
+}
+console.log(data.reduce(reducer2)); // -6.583333333333333
+
+function explanation(total, value) {
+  return `(${total} / ${value} - ${value})`; //
+}
+console.log(data.reduce(explanation)); // ((2 / 4 - 4) / 6 - 6)
